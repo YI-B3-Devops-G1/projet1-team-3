8970eb612d6d1ab9af4b221e8d0c0cd3936f2f1a
Adeline Delbecq <redwappin>
2020-02-04 16:38:26 +0100

    bruh

--- .circleci/scripts/docker_tag_push.sh
+++ .circleci/scripts/docker_tag_push.sh
@@ -4,8 +4,7 @@ IMAGE_TAG=$(.circleci/scripts/get_tag.sh)
 echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 docker tag "project_api":"latest" "redwappin"/"projet1_ade":$IMAGE_TAG
 docker push "redwappin"/"projet1_ade":$IMAGE_TAG
-docker push "redwappin"/"projet1_ade":latest
 if [ "${CIRCLE_TAG}" != "" ];
 then
-    docker push "redwappin"/"projet1_ade":latest
+    docker push "redwappin"/"projet1_ade":"latest"
 fi
