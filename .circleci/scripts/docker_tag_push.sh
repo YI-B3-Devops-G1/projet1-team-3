37fbfbd287764779952996bb560ee471989c81e0
Adeline Delbecq <redwappin>
2020-02-04 17:49:52 +0100

    bruh

--- .circleci/scripts/docker_tag_push.sh
+++ .circleci/scripts/docker_tag_push.sh
@@ -1,6 +1,6 @@
 IMAGE_TAG=$(.circleci/scripts/get_tag.sh)
 echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-docker tag $PROJECT_NAME:"latest" $IMAGE_NAME:$IMAGE_TAG
+docker tag "project_api":"latest" $IMAGE_NAME:$IMAGE_TAG
 docker push $IMAGE_NAME:$IMAGE_TAG
 if [ "${CIRCLE_TAG}" != "" ];
 then
