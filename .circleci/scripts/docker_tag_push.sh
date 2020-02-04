88f6c396fc0d64e8affd50158bc98a746a58fe5b
Adeline Delbecq <redwappin>
2020-02-04 16:52:49 +0100

    bruh

--- .circleci/scripts/docker_tag_push.sh
+++ .circleci/scripts/docker_tag_push.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 IMAGE_TAG=$(.circleci/scripts/get_tag.sh)
 echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 docker tag "project_api":"latest" "redwappin"/"projet1_ade":$IMAGE_TAG
